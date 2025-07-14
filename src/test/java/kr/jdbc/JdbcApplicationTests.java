15245e943b0c616ff07af6360bf895b4aaa702ae
msk226 <kminseok5167@gmail.com>
2025-07-14 13:21:49 +0900

    chore: 초기 파일 업로드

---
+++ src/test/java/kr/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
