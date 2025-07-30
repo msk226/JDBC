0d7d74df0337fcd37bc4218d7adf77d5c057dc10
msk226 <kminseok5167@gmail.com>
2025-07-30 20:17:42 +0900

    feat: 로컬 DB 관련 상수 정의

---
+++ src/main/java/kr/jdbc/common/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package kr.jdbc.common.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "5167";
+}
