e68f837a28fc199baf0e8aa1ddf2b7d39113f487
msk226 <kminseok5167@gmail.com>
2025-07-30 20:31:53 +0900

    feat: 계좌 이체 관련 서비스 로직 테스트 코드 검증 (정상 이쳬)

---
+++ src/test/java/kr/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,61 @@
+package kr.jdbc.service;
+
+import static kr.jdbc.common.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import kr.jdbc.common.connection.ConnectionConst;
+import kr.jdbc.member.domain.Member;
+import kr.jdbc.repository.MemberRepositoryV1;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+class MemberServiceV1Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10_000);
+        Member memberB = new Member(MEMBER_B, 10_000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        memberService.accountTransfer(MEMBER_A, MEMBER_B, 5_000);
+
+        // then
+        Member findMemberA = memberRepository.findById(MEMBER_A);
+        Member findMemberB = memberRepository.findById(MEMBER_B);
+        assertThat(findMemberA.getMoney()).isEqualTo(5_000);
+        assertThat(findMemberB.getMoney()).isEqualTo(15_000);
+    }
+
+
+}
